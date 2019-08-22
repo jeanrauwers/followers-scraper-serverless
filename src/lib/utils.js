52da3acd837e9ed294effdbc4cf936b88186a310
Jean Rauwers <jeanrauwers@gmail.com>
2019-08-22 08:41:33 +0100

    Changes for sort

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,16 +1,17 @@
 export function timeHelper(isToday = false) {
     const date = new Date()
-    
     const currentDay = ('0' + date.getDate()).slice(-2)
     const currentMonth = ('0' + (date.getMonth() + 1)).slice(-2)
     const today = `${currentDay}${currentMonth}${date.getFullYear()}`
-    const currentTime = `${('0' + date.getHours()).slice(-2)}${date.getMinutes()}${date.getSeconds()}`
+    const hour = ('0' + date.getHours()).slice(-2)
+    const currentTime = `${hour}${date.getMinutes()}${date.getSeconds()}`
     if (isToday) return today
+
     return currentTime
 }
 
 export function isInLastSixHours(timestamp) {
-    const defineTimer = 1000 * 60 * 60 * 6// 6 hour
+    const defineTimer = 1000 * 60 * 60 * 6 // 6 hour
     if (timeHelper() - timestamp < defineTimer) {
         return true
     }
@@ -25,7 +26,7 @@ export function isFromTheSameDayAndUnderSixHours(aggregateScrapes) {
         if (item.date === timeHelper(true) && isInLastSixHours(item.updatedAt))
             result.push(item)
     })
-    result.sort((a,b) => a.updatedAt - b.updatedAt).reverse()
+    result.sort((a, b) => a.updatedAt < b.updatedAt)
 
     return result
 }
