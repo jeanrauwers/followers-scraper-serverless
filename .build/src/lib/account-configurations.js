f3d3bd17e31ecf032c9af32304c93e4b94c46667
Jean Rauwers <jeanrauwers@gmail.com>
2019-12-02 13:15:47 +0000

    Delete build from branch

--- .build/src/lib/account-configurations.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var accountConfig = {
-    twitterUrl: 'https://twitter.com/jeanrauwers',
-    instagramUlr: 'https://instagram.com/dev.jeanrauwers',
-    youtubeUrl: 'https://www.youtube.com/user/jeanrauwers'
-};
-exports.default = accountConfig;
-//# sourceMappingURL=account-configurations.js.map
